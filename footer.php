84d9fb4928d7238144e2b7f167e2916eaa69f67a
Eliezer Wohl <eliezerwohl@gmail.com>
2018-01-09 15:51:46 -0500

    finished css for frontPage

--- footer.php
+++ footer.php
@@ -3,13 +3,12 @@
 	<div class="contact">
 		<a class="contact-me" target="_blank" href="mailto:<?php the_field('email','option'); ?>"><i class="fa fa-envelope-o" aria-hidden="true"></i> Email</a>
 		<a class="contact-me" target="_blank" href="mailto:<?php the_field('email','option'); ?>"><i class="fa fa-map-marker" aria-hidden="true"></i> 623 Georges Rd., North Brunswick, NJ 08902</a>
-		<a class="contact-me" target="_blank" href="mailto:<?php the_field('email','option'); ?>"><i class="fa fa-phone" aria-hidden="true"></i> 908-698-9164</a>
-
+		<a class="contact-me" target="_blank" href="mailto:<?php the_field('email','option'); ?>"><i class="fa fa-phone" aria-hidden="true"></i> 	908-698-9164</a>
 	</div>
 	<div>
-		<span>Copyright <?php echo date("Y"); ?> Barbara L. Nelson.  All rights reserved</span><span class="divider">|</span><span class="footer-item">Website by <a target="_blank" href="https://www.eandvdesign.com">E and V Design</a></span> </div>
+		<span>Copyright <?php echo date("Y"); ?> Barbara L. Nelson.  All rights reserved</span><span class="divider">|</span><span class="footer-item">Website by <a target="_blank" href="https://www.eandvdesign.com">E and V Design</a></span> 
+	</div>
 	<?php wp_footer(); ?>
-
 	</footer>
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
