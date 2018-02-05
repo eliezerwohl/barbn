2e5da672362d9a80a20d8f9e69610bf8f9d5c4b9
Eliezer Wohl <eliezerwohl@gmail.com>
2018-02-05 11:16:49 -0500

    services page

--- my-approach.php
+++ my-approach.php
@@ -6,12 +6,9 @@
     <h1>My Approach</h1>
     <p>I am a bilingual licensed clinical social worker with a private practice in North Brunswick, NJ, who has been in the caring and helping professions in the healthcare arena since 1993. Therapy sessions are conducted in English and Spanish. I specialize in working with individuals who have experienced trauma. My focus in this area is twofold: childhood sexual abuse (victims and nonoffending parents) and eating/weight problems. Just as importantly, I also work with individuals who are experiencing depression, anxiety, grief and loss, family issues, stage of life issues, and sexuality issues.</p>
     <p>I work with individuals of all ages and stages of life, including children from 6 to 12 years old, preteens from 11 to 13 years old, adolescents/teenagers from 14 to 19 years old, adults 20 years old and older, and older adults 65 and older. My practice is in a central area, close to public transportation.</p>
-
   </div>
   <div class="second text-center">
     <a class="btn btn-default">Contact Me</a>
     <img class="img-hero" src="<?php bloginfo('template_url'); ?>/img/michael-browning.jpg">
 </div>
-
-
 <?php get_footer(); ?>
--- services.php
+++ services.php
@@ -1,4 +1,27 @@
 <?php /* Template Name: Services */ ?>
 <?php get_header(); ?>
+<div class="container container-main">
+  <div class="primary">
+    <div class="services">
+      <h1>Services</h1>
+      <ul>
+        <li>Individual Psychotherapy<span class="divider">|</span></li>
+        <li>Individual Psychotherapy<span class="divider">|</span></li>
+        <li>Individual Psychotherapy<span class="divider">|</span></li>
+        <li>Individual Psychotherapy</li>
+      </ul>
+    </div>
+    <div class="info">
+      <h2>Financial Information</h2>
+      <p><span class="bold">Rates: </span>140-175 for a 50 min session</p>
+      <p><span class="bold">Rates: </span>140-175 for a 50 min session</p>
+      <p><span class="bold">Rates: </span>140-175 for a 50 min session</p>
+      <p><span class="bold">Rates: </span>140-175 for a 50 min session</p>
+      <p><span class="bold">Rates: </span>140-175 for a 50 min session</p>
+    </div>
+  </div>
+  <div class="second text-center">
+    <img class="img-hero" src="<?php bloginfo('template_url'); ?>/img/michael-browning.jpg">
+</div>
 
 <?php get_footer(); ?>
