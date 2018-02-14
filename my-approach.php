6b5f4803e687397ff8c6045eb2a51bfa6c19bfd7
Eliezer Wohl <eliezerwohl@gmail.com>
2018-02-14 13:43:39 -0500

    my approach acf

--- my-approach.php
+++ my-approach.php
@@ -1,14 +1,16 @@
 <?php /* Template Name: My Approach */ ?>
 <?php get_header(); ?>
-<?php get_header(); ?>
 <div class="container container-main">
   <div class="primary">
-    <h1>My Approach</h1>
-    <p>I am a bilingual licensed clinical social worker with a private practice in North Brunswick, NJ, who has been in the caring and helping professions in the healthcare arena since 1993. Therapy sessions are conducted in English and Spanish. I specialize in working with individuals who have experienced trauma. My focus in this area is twofold: childhood sexual abuse (victims and nonoffending parents) and eating/weight problems. Just as importantly, I also work with individuals who are experiencing depression, anxiety, grief and loss, family issues, stage of life issues, and sexuality issues.</p>
-    <p>I work with individuals of all ages and stages of life, including children from 6 to 12 years old, preteens from 11 to 13 years old, adolescents/teenagers from 14 to 19 years old, adults 20 years old and older, and older adults 65 and older. My practice is in a central area, close to public transportation.</p>
+    <h1><?php the_field("my_approach_header"); ?></h1>
+    <?php the_field("my_approach_text"); ?>
   </div>
   <div class="second text-center">
-    <a class="btn btn-default">Contact Me</a>
-    <img class="img-hero" src="<?php bloginfo('template_url'); ?>/img/michael-browning.jpg">
+    <a href="<?php the_field("contact_me_link"); ?>" class="btn btn-default">
+      <?php the_field("contact_me_text"); ?>
+    </a>
+    <?php $image = get_field('image'); ?>
+    <?php if($image){ ?> <img  class="img-hero" src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>">
+    <?php } ?>
 </div>
 <?php get_footer(); ?>
