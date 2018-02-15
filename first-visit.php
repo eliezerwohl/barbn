26b4706bbfe1f6a87ecef748451eb83b261e3967
Eliezer Wohl <eliezerwohl@gmail.com>
2018-02-15 18:43:54 -0500

    acf complete

--- first-visit.php
+++ first-visit.php
@@ -13,15 +13,20 @@
     <div class="helpful">
       <h2><?php the_field("helpful_forms_header"); ?></h2>
       <?php the_field("helpful_forms_text"); ?>
+      <p class="warning bold"><?php if (lang() == "en"){ ?>
+        <?php the_field("eng-emergency", "options"); ?>
+      <?php } else { ?>
+        <?php the_field("esp-emergency", "options"); ?>
+      <?php } ?></p>
     </div>
     <div class="forms">
       <?php if( have_rows( 'helpful_repeater') ){ ?>
         <?php $counter = 0; $rowCount=count( get_field( 'helpful_repeater' ) ); ?>
         <?php while ( have_rows( 'helpful_repeater') ) { the_row(); $counter++ ?>
           <?php if (get_sub_field("file")){ ?>
-            <a download href="<?php the_sub_field("file"); ?>"><?php the_sub_field("text"); ?></a>
+            <a download href="<?php the_sub_field("file"); ?>"><span class="bold"><?php the_sub_field("text"); ?></span></a>
           <?php } else { ?>
-          <?php the_sub_field("text"); ?>
+          <p class="italic"><?php the_sub_field("text"); ?></span></p>
           <?php } ?>
         <?php  } ?>
       <?php }; ?>
--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<div class="contact">
 		<a class="contact-me" target="_blank" href="mailto:<?php the_field('email','option'); ?>"><i class="fa fa-envelope-o" aria-hidden="true"></i> <?php the_field("e-mail", "option"); ?></a>
 		<a class="contact-me" target="_blank" href="<?php the_field("google_map_link", "option"); ?>"><i class="fa fa-map-marker" aria-hidden="true"></i> <?php the_field("address", "option"); ?></a>
-		<a class="contact-me" target="_blank" href="tel:<?php the_field("phone", "option"); ?>"><i class="fa fa-phone" aria-hidden="true"></i>  	<?php the_field("phone", "option"); ?></a>
+		<a class="contact-me" target="_blank" href="tel:<?php the_field("phone", "option"); ?>"><i class="fa fa-phone" aria-hidden="true"></i> <?php the_field("phone", "option"); ?></a>
 	</div>
 	<div>
 		<span class="bottom-contact">&copy; <?php echo date("Y"); ?> Barbara L. Nelson.  All rights reserved</span><span class="divider">|</span><span class="bottom-contact">Website by <a target="_blank" href="https://www.eandvdesign.com">E and V Design</a></span>
